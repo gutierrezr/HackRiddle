600fe05a6a68642eafd1d332b4ffc3e5a0121ca1
School <School@Michaels-MacBook-Pro-4.local>
2017-11-11 14:47:05 -0500

    Added button, and main screen

--- Riddle/ViewController.swift
+++ Riddle/ViewController.swift
@@ -1,25 +1,83 @@
 //
-//  ViewController.swift
-//  Riddle
 //
-//  Created by Robert Gutierrez on 11/11/17.
-//  Copyright © 2017 Robert Gutierrez. All rights reserved.
+//  Created by Michael Strohmeier on 11/11/17.
+//  Copyright © 2017 Michael Strohmeier. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+let closedCircle = UIImageView(image: UIImage(named: "closedCircle.png")) // Please change me later
+let openCircle = UIImageView(image: UIImage(named: "openCircle.png")) // Please change me later
+var inCorrectArea = true
 
+//var closedDot
+
+let label = UILabel(frame: CGRect(x: 0, y: 0, width: 200, height: 21))
+
+class ViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        openCircle.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        openCircle.center = CGPoint(x: self.view.frame.width / 2, y: self.view.frame.height / 2 + 200)
+        view.addSubview(openCircle)
+        
+        closedCircle.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        closedCircle.center = CGPoint(x: self.view.frame.width / 2, y: self.view.frame.height / 2 - 200)
+        view.addSubview(closedCircle)
+        
+        label.center = CGPoint(x: self.view.frame.width / 2, y: self.view.frame.height / 2)
+        label.textAlignment = .center
+        label.text = "Touch the dot"
+        self.view.addSubview(label)
+        
+        
     }
-
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        label.isHidden = false
+        label.text = "Move to open circle"
+        
+        if let touch = touches.first {
+            let position = touch.location(in: view)
+            print(position)
+        }
+        
+        // IF we touched the first dot then
+        // randomly show another dot
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if (!inCorrectArea) {
+            // Prompt user to touch screen again
+        }
+        label.isHidden = false
+    }
+    
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        // if we are at the open circle do something
+        if let touch = touches.first {
+            let position = touch.location(in: view)
+            print(position)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
-
+    
+    
+    
+    
+    //    func generateDot() {
+    //
+    //        let x_cord = arc4random_uniform(self.view.frame.width - button.size)
+    //        let y_cord = arc4random_uniform(self.view.frame.height - dot.size)
+    //    }
+    
+    
 }
 
